4a64e11ee16531cf3cc489d102dc7986936dc119
Ahmed-ES-SH <ahmedshehata9889@gmail.com>
2026-08-18 17:16:31 +0300

    fix(home): hide blog section and extend warranty to 3 months

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -23,7 +23,7 @@ export default async function Home() {
       <FaqSection />
       <GovermentsLogos />
       {/* <TestimonialsSection /> */}
-      <BlogSection />
+      {/* <BlogSection /> */}
     </>
   );
 }
